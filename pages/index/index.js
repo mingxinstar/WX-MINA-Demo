2614fff577dbcab2f182981a6cd57d7131ca336a
mingxin.huang <mingxinstar.huang@gmail.com>
2016-10-09 17:42:27 +0800

    开始更改为么么直播微信小程序demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,28 +1,45 @@
 //index.js
 //获取应用实例
 var app = getApp()
+const apiUrl = 'http://api.memeyule.com/public/room_recomm?size=8';
+
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-    // wx.redirectTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-        console.log(userInfo);
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
-})
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        liveList : []
+    },
+    //事件处理函数
+    bindViewTap: function() {
+        wx.navigateTo({
+            // wx.redirectTo({
+            url: '../logs/logs'
+        })
+    },
+    gotoLivePage : (event) => {
+        console.log(event.currentTarget.dataset)
+    },
+    onLoad: function () {
+        console.log('onLoad')
+        var that = this
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function(userInfo){
+            console.log(userInfo);
+            //更新数据
+            that.setData({
+                userInfo:userInfo
+            })
+        })
+
+        wx.request({
+            url : apiUrl,
+            success : (res) => {
+                console.log(res);
+
+                that.setData({
+                    liveList : res.data.data
+                })
+            }
+        })
+    }
+});
